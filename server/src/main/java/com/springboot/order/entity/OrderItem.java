99fba7f027e6c26a10f08e9c7d4a783277cf5017
wonil112 <nicewonil@naver.com>
2024-09-06 19:36:25 +0900

    feat OrderItemRepository(#15)

--- server/src/main/java/com/springboot/order/entity/OrderItem.java
+++ server/src/main/java/com/springboot/order/entity/OrderItem.java
@@ -1,6 +1,5 @@
 package com.springboot.order.entity;
 
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
---
+++ server/src/main/java/com/springboot/order/repository/OrderItemRepository.java
@@ -0,0 +1,8 @@
+package com.springboot.order.repository;
+
+import com.springboot.order.entity.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+
+}
