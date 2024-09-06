06e1fe8dbb25dfa02e2587215a6d17072f02775d
skyla00 <hanulofu@gmail.com>
2024-09-06 19:23:34 +0900

    feat MemberController(#19)

---
+++ server/src/main/java/com/springboot/member/controller/MemberController.java
@@ -0,0 +1,34 @@
+package com.springboot.member.controller;
+
+import com.springboot.member.entity.Member;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.net.URI;
+
+@RestController
+@RequestMapping("/members")
+@Validated
+public class MemberController {
+    private final static String MEMBER_DEFAULT_URL = "/members";
+    private final MemberService memberService;
+    private final MemberMapper memberMapper;
+
+    public MemberController(MemberService memberService, MemberMapper mapperMapper) {
+        this.memberService = memberService;
+        this.mapperMapper = mapperMapper;
+    }
+
+    @PostMapping
+    public ResponseEntity postMember(@Valid @RequestBody MemberDto.Post requestBody) {
+        Member member = memberMapper.memberPostDtoToMember(requestBody);
+        Member createMember = memberService.createMember(member);
+        URI location = UriCreator.createUri(MEMBER_DEFAULT_URL, createMember.getMemberId());
+        return ResponseEntity.created(location).build();
+    }
+}
