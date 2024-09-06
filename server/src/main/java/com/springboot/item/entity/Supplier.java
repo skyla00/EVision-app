4a22f55c480c882ffdf8bfd1512ca97f1d6825b8
KoKyungBeom <kyungbum6805@gmail.com>
2024-09-06 18:15:55 +0900

    feat ItemService(#29)

--- server/src/main/java/com/springboot/item/entity/Supplier.java
+++ server/src/main/java/com/springboot/item/entity/Supplier.java
@@ -1,23 +1,34 @@
 package com.springboot.item.entity;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Supplier {
     @Id
     private String supplierCode;
+
     @Column(nullable = false,length = 30)
     private String supplierName;
+
     @Column(nullable = false,length = 50)
     private String supplierAddress;
+
     @Column(nullable = false,length = 13)
     private String supplierPhone;
+
     @Column(nullable = false,length = 30)
     private String supplierEmail;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 30)
+    private SupplierStatus supplierStatus = SupplierStatus.ACTIVE;
 
     public enum SupplierStatus {
         ACTIVE("이용가능"),
