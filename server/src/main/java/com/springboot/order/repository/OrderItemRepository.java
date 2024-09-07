2497c7c0ecb939b585f82e6bc743454998f263ac
wonil112 <nicewonil@naver.com>
2024-09-08 01:01:07 +0900

    feat OrderHeaderHistoryRepository(#13)

--- server/src/main/java/com/springboot/order/repository/OrderItemRepository.java
+++ server/src/main/java/com/springboot/order/repository/OrderItemRepository.java
@@ -1,8 +1,11 @@
 package com.springboot.order.repository;
 
+import com.springboot.order.entity.OrderHeader;
 import com.springboot.order.entity.OrderItem;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
-
+    Page<OrderItem> findAll(Pageable pageable);
 }
