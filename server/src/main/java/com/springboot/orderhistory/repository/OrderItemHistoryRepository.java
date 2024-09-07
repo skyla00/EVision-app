b526453462059e903246bce41882f9c9f374a816
wonil112 <nicewonil@naver.com>
2024-09-08 03:34:19 +0900

    feat OrderHistoryService(#12)

--- server/src/main/java/com/springboot/orderhistory/repository/OrderItemHistoryRepository.java
+++ server/src/main/java/com/springboot/orderhistory/repository/OrderItemHistoryRepository.java
@@ -4,7 +4,9 @@ import com.springboot.orderhistory.entity.OrderItemHistory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OrderItemHistoryRepository extends JpaRepository<OrderItemHistory, Long> {
     Page<OrderItemHistory> findAll(Pageable pageable);
 }
