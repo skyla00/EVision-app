b3b99f8e03eaae307fc225490d6899d94b34fd7b
skyla00 <hanulofu@gmail.com>
2024-09-19 23:45:20 +0900

    fix deleteButton

--- client/src/component/Customer/CustomerSearchInfo.js
+++ client/src/component/Customer/CustomerSearchInfo.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import CustomerSearchInfoList from './CustomerSearchInfoList';
 import { AuthContext } from '../../auth/AuthContext'; // AuthContext import
+import axios from 'axios';
 
 const CustomerSearchInfo = ({ title, headers, customers = [], onOpenPostModal, onOpenModifyModal, onSelectCustomer, selectedCustomer }) => {
     const { userInfo } = useContext(AuthContext); // AuthContext에서 userInfo 가져오기
@@ -20,6 +21,30 @@ const CustomerSearchInfo = ({ title, headers, customers = [], onOpenPostModal, o
             alert('수정할 판매업체를 선택하세요.'); // 선택된 판매업체가 없을 때 경고
         }
     };
+    const handleDeleteClick = async () => {
+        if (selectedCustomer) {
+            let message = window.confirm('정말 삭제하시겠습니까?')
+            if(message) {
+                let accessToken = window.localStorage.getItem('accessToken');
+                try {
+                    const response = await axios.delete(process.env.REACT_APP_API_URL + 'customers' + '/' + selectedCustomer.customerCode,
+                        {   headers: {
+                                Authorization: `${accessToken}`,
+                                'Content-Type': 'application/json',
+                            }
+                        });
+                        if (response.status === 204) {
+                            window.location.reload();
+                        }
+                } catch (error) { 
+                    console.error(error.response.data.message);
+                }
+
+            }
+        } else {
+            alert('삭제할 판매업체를 선택하세요.'); // 선택된 상품이 없을 때 경고
+        }
+    }
 
     return (
         <div className="search-info-container">
@@ -30,6 +55,7 @@ const CustomerSearchInfo = ({ title, headers, customers = [], onOpenPostModal, o
                         <>
                             <button className="order-button" onClick={onOpenPostModal}> 등록 </button>
                             <button className="modify-button" onClick={handleModifyClick}> 수정 </button>
+                            <button className="modify-button" onClick={handleDeleteClick}> 삭제 </button>
                         </>
                     )}
                 </div>
