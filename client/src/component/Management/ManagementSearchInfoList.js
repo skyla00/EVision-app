0bb763018a99b38fde8c493d08b2e22d19135e17
skyla00 <hanulofu@gmail.com>
2024-09-20 16:03:00 +0900

    feat createHistoryModal

--- client/src/component/Management/ManagementSearchInfoList.js
+++ client/src/component/Management/ManagementSearchInfoList.js
@@ -1,31 +1,56 @@
-import React from "react";
+import React, {useState} from "react";
 import ManagementOrderSearchInfoItem from './ManagementOrderSearchInfoItem.js'
+import ManagementHistoryModal from '../../Modal/Management/ManagementHistoryModal.js'
 
 const ManagementSearchInfoList = ({ managementOrders, headerKey, headers, onSelectOrder, selectedOrder,  onToggleFavorite, favorites}) => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [selectedOrderId, setSelectedOrderId] = useState(null);
+
+    const handleOpenModal = (orderHeaderId) => {
+        setSelectedOrderId(orderHeaderId);
+        setIsModalOpen(true);
+    };
+
+    const handleCloseModal = () => {
+        setIsModalOpen(false);
+        setSelectedOrderId(null);
+    };
+
     return (
-        <table className="search-info-table">
-            <thead>
-                <tr>
-                    {headers.map((header) => (
-                        <th key={header.value}>{header.label}</th>
+        <>
+            <table className="search-info-table">
+                <thead>
+                    <tr>
+                        {headers.map((header) => (
+                            <th key={header.value}>{header.label}</th>
+                        ))}
+                    </tr>
+                </thead>
+                <tbody>
+                    {managementOrders.map((managementOrder, index) => (
+                        <ManagementOrderSearchInfoItem 
+                        key={managementOrder.orderHeaderId} 
+                        managementOrder={managementOrder} 
+                        index={index} 
+                        onSelectOrder={onSelectOrder}
+                        isSelected={selectedOrder?.orderHeaderId === managementOrder.orderHeaderId}
+                        headerKey={headerKey} 
+                        onToggleFavorite={onToggleFavorite}
+                        favorites={favorites}
+                        onOpenModal={handleOpenModal}
+                    />
                     ))}
-                </tr>
-            </thead>
-            <tbody>
-                {managementOrders.map((managementOrder, index) => (
-                    <ManagementOrderSearchInfoItem 
-                    key={managementOrder.orderHeaderId} 
-                    managementOrder={managementOrder} 
-                    index={index} 
-                    onSelectOrder={onSelectOrder}
-                    isSelected={selectedOrder?.orderHeaderId === managementOrder.orderHeaderId}
-                    headerKey={headerKey} 
-                    onToggleFavorite={onToggleFavorite}
-                    favorites={favorites}
+                </tbody>
+            </table>
+            {isModalOpen && (
+                <ManagementHistoryModal
+                    isOpen={isModalOpen}
+                    onClose={handleCloseModal}
+                    orderId={selectedOrderId}  // Pass order ID if necessary
                 />
-                ))}
-            </tbody>
-        </table>
+            )}
+        </>
+        
     );
 };
 
